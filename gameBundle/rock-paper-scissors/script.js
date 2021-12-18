93c95561ef027a19d722957f523d7dbd5ab5efbe
Zia <zeazzzz4@gmail.com>
2021-12-18 11:57:19 +0530

    games changes accomplished

--- gameBundle/rock-paper-scissors/script.js
+++ gameBundle/rock-paper-scissors/script.js
@@ -1,15 +1,23 @@
-const userChoice = document.createElement("h3");
-const computerChoice = document.createElement("h3");
+const userChoice = document.createElement("div");
+const computerChoice = document.createElement("div");
 const verdict = document.createElement("h3");
 const gamesgrid = document.getElementById("games");
 gamesgrid.append(userChoice, computerChoice, verdict);
-const choices = ["rock", "paper", "scissors"];
+const choices = [
+  "url(img/icons8-hand-50.png)",
+  "url(img/icons8-hand-rock-50.png)",
+  "url(img/icons8-hand-scissors-50.png)",
+];
+// document.querySelector(".user-img").style.backgroundImage = choices[0];
+// document.querySelector(".computer-img").style.backgroundImage = choices[0];
 
 const clickHandler = (e) => {
-  userChoice.innerHTML = "User Choice:" + e.target.innerHTML;
+  document.querySelector(".user-img").style.backgroundImage =
+    choices[e.target.id];
   const randomVal = Math.floor(Math.random() * 3);
-  computerChoice.innerHTML = "Computer Choice:" + choices[randomVal];
-  if (e.target.innerHTML === choices[randomVal]) {
+  document.querySelector(".computer-img").style.backgroundImage =
+    choices[randomVal];
+  if (parseInt(e.target.id) === randomVal) {
     document.getElementById("result").innerHTML =
       "Great!!! YOU WON! wanna try again ;)";
   } else {
@@ -19,9 +27,9 @@ const clickHandler = (e) => {
 };
 for (let i = 0; i < choices.length; i++) {
   const btn = document.createElement("button");
-  btn.id = choices[i];
-  btn.innerHTML = choices[i];
+  btn.id = i;
+  btn.style.backgroundImage = choices[i];
   gamesgrid.appendChild(btn);
   console.log(btn.id);
-  document.getElementById(choices[i]).addEventListener("click", clickHandler);
+  document.getElementById(i).addEventListener("click", clickHandler);
 }
